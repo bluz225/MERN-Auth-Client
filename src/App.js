23e25313223600b3650cddafd24c6210e5ab5d9b
Billy Lu <billyfwlu@gmail.com>
2022-06-22 16:59:03 -0700

    initial client setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { useState, useEffect } from "react"
+import Login from './components/pages/Login';
+import Profile from './components/pages/Profile';
+import Register from './components/pages/Register';
+import Welcome from './components/pages/Welcome';
+import Navbar from './components/Navbar';
 
 function App() {
+  // the currently logged in user will be stored in state
+  const [currentUser, setCurrentUser] = useState(null)
+
+  //useEffect -- if the user navigates away from the page, we will log them back in
+
+  // event handler to log the user out when needed
+
+  const handleLogout = () => {
+    console.log("log the user out")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <Navbar
+          currentUser = {currentUser}
+          handleLogout = {handleLogout}
+        />
       </header>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Welcome
+            currentUser={currentUser}
+            setCurrentUser = {setCurrentUser}
+          />}/>
+
+          <Route path="/register" element={<Register
+            currentUser={currentUser}
+            setCurrentUser = {setCurrentUser}
+          />}/>
+
+          <Route path="/login" element={<Login
+            currentUser={currentUser}
+            setCurrentUser = {setCurrentUser}
+          />}/>
+
+          <Route path="/profile" element={<Profile
+            currentUser={currentUser}
+            setCurrentUser = {setCurrentUser}
+          />}/>
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
